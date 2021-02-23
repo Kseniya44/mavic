125727b0cc434828bc7d10bfe4cefa3d70c8078c
kseniya <kseniya.for.email@gmail.com>
2021-02-23 22:10:10 +0200

    Styles for header were added

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
-$(function(){
+//scroll header
+const scrollHeader = () => {
+    const header = document.querySelector(".header");
+    if(window.scrollY >= 200) {
+        header.classList.add("header-bg");
+    }else {
+        header.classList.remove("header-bg");
+    }
+}
 
-
-
-  
-});
+window.addEventListener("scroll", scrollHeader);
